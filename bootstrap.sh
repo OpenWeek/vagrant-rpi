d78a554f14a20aac645af5c01a0737ed62306af4
nrybowski <nicolas.rybowski@student.uclouvain.be>
2019-06-27 16:22:13 +0200

    [bootstrap.sh] Update data-logger group permissions

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,18 +1,23 @@
-echo "@edge http://nl.alpinelinux.org/alpine/edge/main" >> /etc/apk/repositories
+grep "@edge" /etc/apk/repositories >> /dev/null
+if [ $? -ne 0 ]
+then
+	echo "@edge http://nl.alpinelinux.org/alpine/edge/main" >> /etc/apk/repositories
+fi
+
 apk add --upgrade apk-tools@edge
 
 apk update
 
 apk upgrade
 
-apk add lighttpd
+apk add lighttpd hostapd dnsmasq mosquitto
 apk add python3 python3-dev py-pip
 apk add git
 apk add g++ gcc make
 apk add rrdtool rrdtool-dev rrdtool-utils
 apk add libxml2 libxml2-dev libxml2-utils
 apk add libxslt libxslt-dev
-apk add linux-headers
+apk add linux-headers shadow
 apk add tree
 
 cat > /home/vagrant/.vimrc << "EOF"
@@ -25,12 +30,18 @@ set noshowmode
 EOF
 
 adduser www -DH
-
-git clone git clone https://github.com/OpenWeek/data-logger.wiki.git
+addgroup data-logger
+usermod -a -G data-logger www
 
 cd /var/www
 git clone https://github.com/OpenWeek/data-logger
-chown vagrant -R data-logger
+git clone https://github.com/OpenWeek/data-logger.wiki.git
+
+chown www:data-logger -R data-logger
+chmod 664 $(find /var/www/data-logger -type f)
+chmod 775 $(find /var/www/data-logger -type d)
+
+chmod 775 data-logger/data_logger/__init__.py
 
 # python2 will be deprecated in 2020 => change all links to python2 by links to python3
 rm /usr/bin/python
